c0cffb208dce3d6ca4a57aa5678e014c3ff7c1cf
Salabh Rai <salabh.rai@gmail.com>
2024-02-13 23:04:34 +0530

    first react app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/Carryminatin" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://facebook.com" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/salabh_2k2" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
